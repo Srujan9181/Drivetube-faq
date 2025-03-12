fedf71bba71f338ed49b1fdcc027a0213a83ae68
g.srujan9181 <g.srujan9181@gmail.com>
2025-03-12 19:12:49 +0000

    13-03-2025

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,30 @@ import './App.css'
 import cs from './assets/customer-service.png'
 import women from './assets/woman.png'
 import worker from './assets/worker.png'
-import { useState } from 'react'
+
 
 function App() {
-  const[arrow,Setarrow]=useState('v')
-  const[id,Setid]=useState('normal')
+
   function handlearrow(e){
 // Suggested code may be subject to a license. Learn more: ~LicenseLog:4129293622.
     let a1=document.getElementById('a1')
     let a2=document.getElementById('a2')
     let a3=document.getElementById('a3')
     let a4=document.getElementById('a4')
+    let arrow1=document.getElementById('q1arrow')
+    let arrow2=document.getElementById('q2arrow')
+    let arrow3=document.getElementById('q3arrow')
+    let arrow4=document.getElementById('q4arrow')
     if (e.target.id==="q1"){
       a1.style.display="block"
       a1.style.marginBottom="20px";
       a2.style.display="none"
       a3.style.display="none"
       a4.style.display="none"
+      arrow1.innerHTML="^"
+      arrow2.innerHTML="v"
+      arrow3.innerHTML="v"
+      arrow4.innerHTML="v"
       
     }
     if (e.target.id==="q2"){
@@ -29,6 +36,10 @@ function App() {
       a1.style.display="none"
       a3.style.display="none"
       a4.style.display="none"
+      arrow1.innerHTML="v"
+      arrow2.innerHTML="^"
+      arrow3.innerHTML="v"
+      arrow4.innerHTML="v"
       
     }
     if (e.target.id==="q3"){
@@ -37,6 +48,10 @@ function App() {
       a1.style.display="none"
       a2.style.display="none"
       a4.style.display="none"
+      arrow1.innerHTML="v"
+      arrow2.innerHTML="v"
+      arrow3.innerHTML="^"
+      arrow4.innerHTML="v"
 
     }
     if (e.target.id==="q4"){
@@ -44,6 +59,10 @@ function App() {
       a1.style.display="none"
       a2.style.display="none"
       a3.style.display="none"
+      arrow1.innerHTML="v"
+      arrow2.innerHTML="v"
+      arrow3.innerHTML="v"
+      arrow4.innerHTML="^"
 
     }
    
@@ -58,13 +77,14 @@ function App() {
     </header>
 
     <main className='faqs'>
-      <div className='question' id="q1" onClick={handlearrow}>What is an Admin Dashbord? <span id='normal'>{arrow}</span></div>
+  
+  <div className='question' id="q1" onClick={handlearrow}>What is an Admin Dashbord? <span id='q1arrow'>v</span></div>
       <div className='answer' id='a1'>Admin Dashboard is the backend interface of a website or an application that helps to manage the websito's overall content and settings. It is widely used by the sito owners to koop track of their website, make changes to their content, and more.</div>
-      <div className='question' id="q2" onClick={handlearrow}>What should an admin dashboard template include? <span  id='normal' >{arrow}</span></div>
+      <div className='question' id="q2" onClick={handlearrow}>What should an admin dashboard template include? <span  id='q2arrow' >v</span></div>
       <div className='answer'id='a2'>ans2</div>
-      <div className='question' id="q3" onClick={handlearrow}>Why should i buy admin templates from AdminMart? <span id='normal' >{arrow}</span></div>
+      <div className='question' id="q3" onClick={handlearrow}>Why should i buy admin templates from AdminMart? <span id='q3arrow' >v</span></div>
       <div className='answer' id='a3'>ans3</div>
-      <div className='question' id="q4" onClick={handlearrow}>Do Adminmart offers a money back gurantee <span id='normal'>{arrow}</span></div>
+      <div className='question' id="q4" onClick={handlearrow}>Do Adminmart offers a money back gurantee <span id='q4arrow'>v</span></div>
       <div className='answer' id='a4'>ans4</div>
     </main>
     <footer>
